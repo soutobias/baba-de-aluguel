e6da2aa7e4bd7349c7b1f7b6da049339379caac2
Glauber Lima <glauberdelima@gmail.com>
2020-10-29 20:51:31 -0300

    Created Authentication, devise, Athorization & pundit

---
+++ app/policies/babysitter_policy.rb
@@ -0,0 +1,7 @@
+class BabysitterPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :views
   devise_for :users
   root to: "pages#home"
   get "profile", to: "dashboard#profile"
