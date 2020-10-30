711089be0b836e9f33d13b0ebf6474e6b17566df
Fred Coque <fredcoque@yahoo.com>
2020-10-30 18:29:47 -0500

    Added skill to babysitters

--- db/schema.rb
+++ db/schema.rb
@@ -69,8 +69,21 @@ ActiveRecord::Schema.define(version: 2020_10_30_004522) do
     t.string "name"
     t.string "phone"
     t.string "location"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "views", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_views_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
