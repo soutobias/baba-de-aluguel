cebe4a2926fc707d4548e3ba3e28e49f1d2f337b
Alice Ratton <aliceratton@gmail.com>
2020-10-29 23:28:28 -0200

    home_page half done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-    @babysitter = Babysitter.new
   end
+
+  def filter
+  end
+
 end
